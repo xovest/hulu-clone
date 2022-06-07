c8ed8b233d5a7d1806fecaaa407ec09056ad358e
xovest <79008917+xovest@users.noreply.github.com>
2022-06-07 17:24:27 +0300

    done with styling and everything

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Results.css'
 import VideoCard from './VideoCard'
+import axios from './axios'
+import FlipMove from 'react-flip-move'
 
-function Results() {
+function Results({ selectedOption }) {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get(selectedOption)
+      setMovies(req.data.results)
+      return req
+    }
+
+    fetchData()
+  }, [selectedOption])
+
   return (
     <div className='results'>
-      <VideoCard />
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard movie={movie} key={movie.id} />
+        ))}
+      </FlipMove>
     </div>
   )
 }
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import './VideoCard.css'
 
-function VideoCard({ movie }) {
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+const VideoCard = forwardRef(({ movie }, ref) => {
   return (
-    <div className='videoCard'>
-      <img src="" alt="img" />
-      <p>Overview</p>
-      <h2>Title</h2>
-      <p>Likes</p>
+    <div ref={ref} className='videoCard'>
+      <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="poster" />
+      <p>{movie.overview}</p>
+      <h2>{movie.title || movie.original_name}</h2>
+      <p className='videoCard__stats'>
+        {movie.media_type && `${movie.media_type} *`}
+        {movie.release_date || movie.first_air_date} *
+        <p>{movie.vote_count}</p>
+      </p>
     </div>
   )
-}
+})
 
 export default VideoCard
