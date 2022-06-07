64294aa5f14e0e6f8681b79b5bacc757f9b214f1
xovest <79008917+xovest@users.noreply.github.com>
2022-06-07 14:52:35 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
-
-      {/* Nav */}
-
-      {/* Results */}
+    <div className="app">
+      <Header />
     </div>
   );
 }
