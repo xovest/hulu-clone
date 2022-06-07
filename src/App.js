7e69087dfc771e8006772a6fabeac41a15ad2bf5
xovest <79008917+xovest@users.noreply.github.com>
2022-06-07 15:08:28 +0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Nav />
     </div>
   );
 }
